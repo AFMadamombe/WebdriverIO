59f4740f61ec3f40c69e24a6af03daff7ef2d4bb
unknown <bcoxmanc100@gmail.com>
2019-10-11 10:06:09 +0100

    1st commit

---
+++ test/specs/login.js
@@ -0,0 +1,26 @@
+const assert = require('assert')
+
+describe('Log in to The Data Refinery', () => {
+    it('Should enable user to login with correct credentials', () => {
+        
+        browser.url('https://thedatarefinery.co.uk')
+        browser.maximizeWindow()
+        const page = browser.$('data-refinery').shadow$('toolbar-menu')
+        const login = page.shadow$('paper-button[class=secondary]')
+        login.click()
+        browser.waitUntil(() => browser.getUrl() === 'https://thedatarefinery.co.uk/login');
+        assert.equal(browser.getUrl(), 'https://thedatarefinery.co.uk/login')
+        browser.pause(1000)
+        const loginPage = browser.$('data-refinery').shadow$('login-page');
+        const email = loginPage.shadow$('input[name="email"]')
+        email.addValue('admin@thedatarefinery.co.uk');
+        const password = loginPage.shadow$('input[name="password"]')
+        password.addValue('m@8ckdC6h');
+        const submit = loginPage.shadow$('form-button')
+        submit.click();
+        browser.waitUntil(() => browser.getUrl() === 'https://thedatarefinery.co.uk/dashboard');
+        assert.equal(browser.getUrl(), 'https://thedatarefinery.co.uk/dashboard')
+
+        browser.closeWindow()
+    })
+})
---
+++ test/specs/signup.js
@@ -0,0 +1,53 @@
+/*const assert = require('assert')
+
+describe('Sign up to The Data Refinery', () => {
+    it('Should enable user to sign up with correct credentials', () => {
+        //login tot the Data Refinery
+        browser.url('https://thedatarefinery.co.uk')
+        browser.maximizeWindow()
+
+        //Locate the shadow root of the element
+        const page = browser.$('data-refinery').shadow$('toolbar-menu')
+
+        //Locate the element 
+        const signUp = page.shadow$('paper-button[class=submit]')
+
+        //click on the element button 
+        signUp.click()
+        browser.pause(3000)
+
+        //Select the Unlimited records, 1 User from the drop down
+        const packageElement = browser.$('data-refinery').shadow$('register-page').shadow$('packages-element').shadow$('package-element')
+        const dropdown = packageElement.shadow$('select')
+        dropdownValue = dropdown.selectByIndex(0)
+        //dropdown.selectByAttribute('value', '1')
+        browser.pause(1000)
+
+        //Click on the Join now button 
+        const formButton = packageElement.shadow$("form-button")
+        formButton.click()
+        browser.pause(2000)
+
+        //Enter sign up
+        const registerTextBox = browser.$("data-refinery").shadow$("register-page").shadow$("register-form")
+
+        const firstName = registerTextBox.shadow$("input-element:nth-child(1)").shadow$('input[id=first_name]')
+        const lastName = registerTextBox.shadow$("input-element:nth-child(2)").shadow$('input[id=last_name]')
+        const companyName = registerTextBox.shadow$("input-element:nth-child(3)").shadow$('input[id=company_name]')
+        const email = registerTextBox.shadow$("input-element:nth-child(4)").shadow$('input[id=email]')
+
+        firstName.addValue('Alan')
+        lastName.addValue('Watts')
+        companyName.addValue('Elf Chatter')
+        email.addValue('alanwatts1221341@gmail.com')
+        browser.pause(5000)
+
+        //Click submit button
+        const submitButton = registerTextBox.shadow$('form-button[id=btn')
+        //submitButton.click()
+        browser.pause(1000)
+              
+
+        browser.closeWindow()
+    })
+})*/
